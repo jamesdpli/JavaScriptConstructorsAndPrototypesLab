0f150add25280e1fb27e7137b1e0b27360a298db
James Dudhi-Price <jamesdudhiprice@gmail.com>
2022-06-01 22:53:28 +0100

    Fix addCarToStock method

--- carDealership.js
+++ carDealership.js
@@ -18,7 +18,41 @@ const Car = function (manufacturer, price, engineType) {
     this.getEngineType = () => _engineType;
 }
 
-const car1 = new Car("Ford", 200000, "V-8");
-console.log("car1 engine type:", car1.getEngineType());
+// New up some cars
+const car1 = new Car("Ford Fiesta", 200000, "V-8");
+const car2 = new Car("Toyota Prius", 10000, "Super charged V-8");
+const car3 = new Car("Volkswagen Polo", 30000, "1.2l");
+const car4 = new Car("Lexus ls250", 500000, "2.9l");
+const car5 = new Car("Dacia Duster", 90000, "Super charged V-8");
+const car6 = new Car("Toyota Auris", 300, "Twin turbo");
+const car7 = new Car("Toyota Avensis", 2020202, "CrazyFastEngine");
 
-module.exports = {car1}
+const Dealership = function (title, maximumNumberOfCars, carsInStock) {
+
+    let _title = title;
+    let _maximumNumberOfCars = maximumNumberOfCars;
+    let _carsInStock = carsInStock;
+
+    this.getTitle = () => title;
+    this.getMaximumNumberOfCars = () => maximumNumberOfCars;
+    this.carsInStock = () => carsInStock;
+
+    this.countNumberOfCarsInStock = () => carsInStock.length;
+    this.addCarToStock = function (manufacturer, price, engineType) {
+        dealership1.carsInStock().push(new Car(manufacturer, price, engineType));
+    }
+    this.getAllCarManufacturers = function () {
+        
+    }
+}
+
+// New up some dealerships
+const dealership1 = new Dealership("Jimmy's Best Cars", 20, [car1, car2, car7, car5]);
+
+console.log(dealership1.countNumberOfCarsInStock());
+dealership1.addCarToStock("Su", 10, "su");
+console.log(dealership1.countNumberOfCarsInStock());
+console.log(dealership1.carsInStock());
+console.log(dealership1.getAllCarManufacturers());
+
+module.exports = {car1, car2, car3, car4, car5, car6, car7}
--- task_constructors_prototypes.md
+++ task_constructors_prototypes.md
@@ -4,16 +4,16 @@ In this exercise we will explore some of the differences between how classes are
 
 ## Task
 
-- Create ` Car` class. The car should have properties representing manufacturer, price and engine type.
-- Set up Jest and write tests to make sure you can access each of those properties in a `Car` object.
-- Create a `Dealership` class. It should have properties representing its name, maximum number of cars it can have and the cars currently in stock.
+- Create ` Car` class. The car should have properties representing manufacturer, price and engine type. ✅
+- Set up Jest and write tests to make sure you can access each of those properties in a `Car` object. ✅
+- Create a `Dealership` class. It should have properties representing its name, maximum number of cars it can have and the cars currently in stock. ✅
 - Add methods to `Dealership` to:
-	- Count the number of cars in stock
-	- Add a car to stock
-	- Return an array containing each car's manufacturer
-	- Find all the cars from a given manufacturer
-	- Find the total value of all the cars in stock
-- Write tests for `Dealership`'s methods.
+	- Count the number of cars in stock ✅
+	- Add a car to stock ✅
+	- Return an array containing each car's manufacturer **PENDING**
+	- Find all the cars from a given manufacturer **PENDING**
+	- Find the total value of all the cars in stock **PENDING**
+- Write tests for `Dealership`'s methods. **PENDING**
 
 ## Extensions
 
